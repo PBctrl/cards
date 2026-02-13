3fa8b89bef71035d89935abc3f67998043f73c59
Reiner Huechting <reinerh-dev@pm.me>
2026-02-13 10:30:21 +0100

    Hand-Package implementiert.

--- deck/deck.go
+++ deck/deck.go
@@ -1,6 +1,11 @@
 package deck
 
-import "cards/card"
+import (
+	"cards/card"
+	"cards/rank"
+	"cards/suit"
+	"math/rand"
+)
 
 // Repräsentiert einen Kartenstapel.
 type Deck struct {
@@ -14,14 +19,31 @@ func Empty() *Deck {
 
 // New32 erzeugt ein neues Skatblatt mit 32 Karten.
 func New32() *Deck {
-	// TODO
-	return &Deck{}
+
+	result := &Deck{}
+	for s := suit.Clubs; s <= suit.Diamonds; s++ {
+		for r := rank.Seven; r <= rank.Ace; r++ {
+			c := card.Card{Rank: r, Suit: s}
+			result.cards = append(result.cards, c)
+		}
+	}
+
+	return result
 }
 
 // New52 erzeugt ein neues französisches Blatt mit 52 Karten.
 func New52() *Deck {
-	// TODO
-	return &Deck{}
+
+	result := &Deck{}
+	for s := suit.Clubs; s <= suit.Diamonds; s++ {
+		for r := rank.Two; r <= rank.Ace; r++ {
+			c := card.Card{Rank: r, Suit: s}
+			result.cards = append(result.cards, c)
+		}
+	}
+
+	return result
+
 }
 
 // Add fügt eine Karte zum Deck hinzu.
@@ -31,19 +53,27 @@ func (d *Deck) Add(c card.Card) {
 
 // Shuffle mischt das Deck.
 func (d *Deck) Shuffle() {
-	// TODO
+
+	// Definiere eine Funktion, die zwei Karten im Deck vertauscht.
+	swap := func(i, j int) {
+		d.cards[i], d.cards[j] = d.cards[j], d.cards[i]
+	}
+
+	// Verwende die eben definierte Swap-Funktion,
+	// um die Karten im Deck zu mischen.
+	rand.Shuffle(len(d.cards), swap)
 }
 
 // Draw zieht die oberste Karte.
 // Entfernt die Karte aus dem Deck und liefert sie zurück.
 func (d *Deck) Draw() card.Card {
-	// TODO
-	return card.Card{}
+	c := d.Top()
+	d.cards = d.cards[:len(d.cards)-1]
+	return c
 }
 
 // Top liefert die oberste Karte.
 // Entfernt sie aber nicht.
 func (d Deck) Top() card.Card {
-	// TODO
-	return card.Card{}
+	return d.cards[len(d.cards)-1]
 }
--- deck/deck_test.go
+++ deck/deck_test.go
@@ -16,6 +16,194 @@ func ExampleEmpty() {
 	// 0
 }
 
+func ExampleNew32() {
+	d := New32()
+
+	fmt.Println(len(d.cards))
+
+	fmt.Println(d.cards[0])
+	fmt.Println(d.cards[1])
+	fmt.Println(d.cards[2])
+	fmt.Println(d.cards[3])
+	fmt.Println(d.cards[4])
+	fmt.Println(d.cards[5])
+	fmt.Println(d.cards[6])
+	fmt.Println(d.cards[7])
+	fmt.Println(d.cards[8])
+	fmt.Println(d.cards[9])
+	fmt.Println(d.cards[10])
+	fmt.Println(d.cards[11])
+	fmt.Println(d.cards[12])
+	fmt.Println(d.cards[13])
+	fmt.Println(d.cards[14])
+	fmt.Println(d.cards[15])
+	fmt.Println(d.cards[16])
+	fmt.Println(d.cards[17])
+	fmt.Println(d.cards[18])
+	fmt.Println(d.cards[19])
+	fmt.Println(d.cards[20])
+	fmt.Println(d.cards[21])
+	fmt.Println(d.cards[22])
+	fmt.Println(d.cards[23])
+	fmt.Println(d.cards[24])
+	fmt.Println(d.cards[25])
+	fmt.Println(d.cards[26])
+	fmt.Println(d.cards[27])
+	fmt.Println(d.cards[28])
+	fmt.Println(d.cards[29])
+	fmt.Println(d.cards[30])
+	fmt.Println(d.cards[31])
+
+	// Output:
+	// 32
+	// {7 ♣}
+	// {8 ♣}
+	// {9 ♣}
+	// {10 ♣}
+	// {J ♣}
+	// {Q ♣}
+	// {K ♣}
+	// {A ♣}
+	// {7 ♠}
+	// {8 ♠}
+	// {9 ♠}
+	// {10 ♠}
+	// {J ♠}
+	// {Q ♠}
+	// {K ♠}
+	// {A ♠}
+	// {7 ♥}
+	// {8 ♥}
+	// {9 ♥}
+	// {10 ♥}
+	// {J ♥}
+	// {Q ♥}
+	// {K ♥}
+	// {A ♥}
+	// {7 ♦}
+	// {8 ♦}
+	// {9 ♦}
+	// {10 ♦}
+	// {J ♦}
+	// {Q ♦}
+	// {K ♦}
+	// {A ♦}
+}
+
+func ExampleNew52() {
+	d := New52()
+
+	fmt.Println(len(d.cards))
+
+	fmt.Println(d.cards[0])
+	fmt.Println(d.cards[1])
+	fmt.Println(d.cards[2])
+	fmt.Println(d.cards[3])
+	fmt.Println(d.cards[4])
+	fmt.Println(d.cards[5])
+	fmt.Println(d.cards[6])
+	fmt.Println(d.cards[7])
+	fmt.Println(d.cards[8])
+	fmt.Println(d.cards[9])
+	fmt.Println(d.cards[10])
+	fmt.Println(d.cards[11])
+	fmt.Println(d.cards[12])
+	fmt.Println(d.cards[13])
+	fmt.Println(d.cards[14])
+	fmt.Println(d.cards[15])
+	fmt.Println(d.cards[16])
+	fmt.Println(d.cards[17])
+	fmt.Println(d.cards[18])
+	fmt.Println(d.cards[19])
+	fmt.Println(d.cards[20])
+	fmt.Println(d.cards[21])
+	fmt.Println(d.cards[22])
+	fmt.Println(d.cards[23])
+	fmt.Println(d.cards[24])
+	fmt.Println(d.cards[25])
+	fmt.Println(d.cards[26])
+	fmt.Println(d.cards[27])
+	fmt.Println(d.cards[28])
+	fmt.Println(d.cards[29])
+	fmt.Println(d.cards[30])
+	fmt.Println(d.cards[31])
+	fmt.Println(d.cards[32])
+	fmt.Println(d.cards[33])
+	fmt.Println(d.cards[34])
+	fmt.Println(d.cards[35])
+	fmt.Println(d.cards[36])
+	fmt.Println(d.cards[37])
+	fmt.Println(d.cards[38])
+	fmt.Println(d.cards[39])
+	fmt.Println(d.cards[40])
+	fmt.Println(d.cards[41])
+	fmt.Println(d.cards[42])
+	fmt.Println(d.cards[43])
+	fmt.Println(d.cards[44])
+	fmt.Println(d.cards[45])
+	fmt.Println(d.cards[46])
+	fmt.Println(d.cards[47])
+	fmt.Println(d.cards[48])
+	fmt.Println(d.cards[49])
+	fmt.Println(d.cards[50])
+	fmt.Println(d.cards[51])
+
+	// Output:
+	// 52
+	// {2 ♣}
+	// {3 ♣}
+	// {4 ♣}
+	// {5 ♣}
+	// {6 ♣}
+	// {7 ♣}
+	// {8 ♣}
+	// {9 ♣}
+	// {10 ♣}
+	// {J ♣}
+	// {Q ♣}
+	// {K ♣}
+	// {A ♣}
+	// {2 ♠}
+	// {3 ♠}
+	// {4 ♠}
+	// {5 ♠}
+	// {6 ♠}
+	// {7 ♠}
+	// {8 ♠}
+	// {9 ♠}
+	// {10 ♠}
+	// {J ♠}
+	// {Q ♠}
+	// {K ♠}
+	// {A ♠}
+	// {2 ♥}
+	// {3 ♥}
+	// {4 ♥}
+	// {5 ♥}
+	// {6 ♥}
+	// {7 ♥}
+	// {8 ♥}
+	// {9 ♥}
+	// {10 ♥}
+	// {J ♥}
+	// {Q ♥}
+	// {K ♥}
+	// {A ♥}
+	// {2 ♦}
+	// {3 ♦}
+	// {4 ♦}
+	// {5 ♦}
+	// {6 ♦}
+	// {7 ♦}
+	// {8 ♦}
+	// {9 ♦}
+	// {10 ♦}
+	// {J ♦}
+	// {Q ♦}
+	// {K ♦}
+	// {A ♦}
+}
+
 func ExampleDeck_Add() {
 	d := Empty()
 
